d3ce9e498ff5d35eee029a0836e12b0092ab5b30
yuzaku <yanuaraudrey55@gmail.com>
2025-05-27 23:32:11 +0700

    update databaseseeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,14 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    // public function run(): void
-    // {
-    //     // User::factory(10)->create();
+    public function run(): void
+    {
+        // User::factory(10)->create();
 
-    //     User::factory()->create([
-    //         'name' => 'Test User',
-    //         'email' => 'test@example.com',
-    //     ]);
-    //     $this->call(UsersTableSeeder::class);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $this->call(UsersTableSeeder::class);
+    }
 }
