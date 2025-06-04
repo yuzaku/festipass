00153b01f750d7cfb98119eafe61fb9302c80b5d
yuzaku <yanuaraudrey55@gmail.com>
2025-06-04 09:53:55 +0700

    feat: add select-payment view

--- routes/ticket.php
+++ routes/ticket.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/select-ticket', '/book-ticket/select-ticket');
 Route::view('/select-ticket/book', '/book-ticket/book-ticket');
 Route::view('/select-ticket/order-details', '/book-ticket/order-details');
+Route::view('/select-ticket/order-details/select-payment', '/book-ticket/select-payment');
