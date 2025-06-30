a520386a8f36a5aa9c5411211aabc0d2deb6a8f1
rizal-ramadhan <rizalramadhan.email@gmail.com>
2025-06-30 14:01:25 +0700

    Fix Revert Update V2

---
+++ database/migrations/2025_06_30_065458_add_password_hash_and_tel_num_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password_hash')->nullable()->after('password');
+            $table->string('tel_num', 15)->nullable()->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['password_hash', 'tel_num']);
+        });
+    }
+};
