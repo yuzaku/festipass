084f8adf06510855d280751678782a3441a822e0
yuzaku <yanuaraudrey55@gmail.com>
2025-06-30 18:36:57 +0700

    feat: add auth to booking ticket

--- routes/ticket.php
+++ routes/ticket.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookTicket\TicketSelectionController;
 use App\Http\Controllers\BookTicket\TicketOrderController;
 
-Route::get('/select-ticket/{event}', [TicketSelectionController::class, 'show'])->name('show.ticket');
-Route::get('/select-ticket/book/{ticket}', [TicketSelectionController::class, 'book'])->name('book.ticket');
-Route::post('/select-ticket/order/{ticket}', [TicketOrderController::class, 'order'])->name('order.ticket');
-Route::get('/select-ticket/order-details/{orderId}', [TicketOrderController::class, 'order_details'])->name('order.details');
-Route::get('/select-ticket/order-details/select-payment/{order}', [TicketOrderController::class, 'selectPayment'])->name('order.payment.form');
-Route::post('/select-ticket/order-details/select-payment/{order}', [TicketOrderController::class, 'savePayment'])->name('order.payment.save');
+Route::get('/select-ticket/{event}', [TicketSelectionController::class, 'show'])->middleware('auth')->name('show.ticket');
+Route::get('/select-ticket/book/{ticket}', [TicketSelectionController::class, 'book'])->middleware('auth')->name('book.ticket');
+Route::post('/select-ticket/order/{ticket}', [TicketOrderController::class, 'order'])->middleware('auth')->name('order.ticket');
+Route::get('/select-ticket/order-details/{orderId}', [TicketOrderController::class, 'order_details'])->middleware('auth')->name('order.details');
+Route::get('/select-ticket/order-details/select-payment/{order}', [TicketOrderController::class, 'selectPayment'])->middleware('auth')->name('order.payment.form');
+Route::post('/select-ticket/order-details/select-payment/{order}', [TicketOrderController::class, 'savePayment'])->middleware('auth')->name('order.payment.save');
 Route::post('/orders/{order}/pay', [TicketOrderController::class, 'pay'])->name('order.pay');
 Route::get('/ticket-detail', function () {
     return view('ticket.ticketdetail1');
